fb9fe86338901856e1781505f0b7621a4fdec672
ZhangMaKe <zhangmake86@gmail.com>
2020-01-06 16:01:30 +0800

    init

---
+++ main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  profile = "default"
+  region  = var.region
+}
+
+resource "aws_iam_role" "terraform_test_lambda_role" {
+  name               = "terraform_test_lambda_role"
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+                "Service": "lambda.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+
+provider "aws" {
+  profile = "default"
+  region  = var.region
+}
+
+resource "aws_lambda_function" "terraform_lambda" {
+  filename      = "../function_code/lambda.py.zip"
+  function_name = "terraform_test_function"
+  role          = module.iam.terraform_test_lambda_role_arn
+  handler       = "lambda.handler"
+  runtime       = "python3.7"
+
+output "terraform_test_lambda_role_arn" {
+  value = aws_iam_role.terraform_test_lambda_role.arn
+}
